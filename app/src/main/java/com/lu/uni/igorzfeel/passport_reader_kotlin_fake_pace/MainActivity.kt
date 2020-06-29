b8554f4e8484527d40a8c7e9638091c57afc8461
Igor <bboyifeel@gmail.com>
2020-06-29 15:35:16 +0200

    empty proj

---
+++ app/src/main/java/com/lu/uni/igorzfeel/passport_reader_kotlin_fake_pace/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lu.uni.igorzfeel.passport_reader_kotlin_fake_pace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
